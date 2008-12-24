f37f5f34e3515179f2e29345f552db7fa62c9fae
Dmitry V. Levin <ldv@altlinux.org>
2008-12-24 00:31:30 +0000

    Update bind from ftp://ftp.isc.org/isc/bind9/bind-9.3.6-P1.tar.gz

--- bind/lib/isc/unix/socket.c
+++ bind/lib/isc/unix/socket.c
@@ -15,7 +15,7 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* $Id: socket.c,v 1.207.2.19.2.60 2008/11/12 04:03:37 marka Exp $ */
+/* $Id: socket.c,v 1.207.2.19.2.62 2008/11/20 00:07:38 jinmei Exp $ */
 
 #include <config.h>
 
@@ -492,7 +492,7 @@ FIX_IPV6_RECVPKTINFO(isc_socket_t *sock)
 
 	if (setsockopt(sock->fd, IPPROTO_IPV6, IPV6_RECVPKTINFO,
 		       (void *)&on, sizeof(on)) < 0) {
-	
+
 		UNEXPECTED_ERROR(__FILE__, __LINE__,
 				 "setsockopt(%d, IPV6_RECVPKTINFO) "
 				 "%s: %s", sock->fd,
@@ -3125,7 +3125,6 @@ watcher(void *uap) {
 						    ISC_LOGCATEGORY_GENERAL,
 						    ISC_LOGMODULE_SOCKET,
 						    ISC_LOG_DEBUG(1),
-						    ISC_LOG_INFO,
 						    "unexpected POLL timeout");
 				}
 				pollstate = poll_active;
@@ -4693,7 +4692,7 @@ isc_socket_ipv6only(isc_socket_t *sock, isc_boolean_t yes) {
 		if (setsockopt(sock->fd, IPPROTO_IPV6, IPV6_V6ONLY,
 			       (void *)&onoff, sizeof(int)) < 0) {
 			char strbuf[ISC_STRERRORSIZE];
-	
+
 			UNEXPECTED_ERROR(__FILE__, __LINE__,
 					 "setsockopt(%d, IPV6_V6ONLY) "
 					 "%s: %s", sock->fd,
