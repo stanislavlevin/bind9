033e3dfa7aa39e81f63cec1b44c0ab18ca5fe2ad
Witold Kręcicki <wpk@isc.org>
2020-02-23 23:50:23 +0100

    rwlock debugging

--- lib/isc/rwlock.c
+++ lib/isc/rwlock.c
@@ -75,11 +75,11 @@ __log(isc_rwlock_t *rwl, bool phase, rtype type) {
 			
 		}
 		int i = atomic_fetch_add_relaxed(&__lt_pos, 1);
-		__lt_pos %= 65536;
-		__locks[__lt_pos].ts = rdtsc();
-		__locks[__lt_pos].phase = phase;
-		__locks[__lt_pos].tid = __my_tid;
-		__locks[__lt_pos].type = type;
+		i %= 65536;
+		__locks[i].ts = rdtsc();
+		__locks[i].phase = phase;
+		__locks[i].tid = __my_tid;
+		__locks[i].type = type;
 	}
 }
 
